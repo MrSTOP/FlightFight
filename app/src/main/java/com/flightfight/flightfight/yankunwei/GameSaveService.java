66d1c538772aca51d081f9ba9f11a91525bd48ac
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2020-06-15 21:39:55 +0800

    重构保存记录以及加载记录的方法

--- app/src/main/java/com/flightfight/flightfight/yankunwei/GameSaveService.java
+++ app/src/main/java/com/flightfight/flightfight/yankunwei/GameSaveService.java
@@ -2,21 +2,19 @@ package com.flightfight.flightfight.yankunwei;
 
 import android.app.Service;
 import android.content.Intent;
-import android.os.Binder;
 import android.os.IBinder;
-
 
 import com.flightfight.flightfight.yankunwei.database.LeaderBoardDAO;
 import com.flightfight.flightfight.yankunwei.database.bean.PlayerRecord;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 public class GameSaveService extends Service {
     public static final String SERVICE_ACTION_SAVE_PLAYER_RECORD = GameSaveService.class.getPackage().getName() + ".action.save.player.record";
     public static final String SERVICE_ACTION_LOAD_ALL_PLAYER_RECORD = GameSaveService.class.getPackage().getName() + ".action.load.player.record.all";
     public static final String SERVICE_ACTION_SAVE_GAME = GameSaveService.class.getPackage().getName() + ".action.save.game.state";
+    public static final String SERVICE_ACTION_LOAD_GAME = GameSaveService.class.getPackage().getName() + ".action.load.game.state";
 
 //    class GameSaveServiceBinder extends Binder {
 //        public GameSaveService getService() {
@@ -40,24 +38,42 @@ public class GameSaveService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         if (Objects.equals(intent.getAction(), SERVICE_ACTION_SAVE_PLAYER_RECORD)) {
-            PlayerRecord playerRecord = (PlayerRecord) intent.getSerializableExtra("playerRecord");
-            if (playerRecord == null) {
-                throw new IllegalArgumentException("PlayerRecord cannot be null");
-            }
-            LeaderBoardDAO leaderBoardDAO = new LeaderBoardDAO(this);
-            boolean result = leaderBoardDAO.insertPlayerRecord(playerRecord);
-            Intent resultIntent = new Intent(SERVICE_ACTION_SAVE_PLAYER_RECORD);
-            resultIntent.putExtra("result", result);
-            sendBroadcast(resultIntent);
+            savePlayerRecord(intent);
         } else if (Objects.equals(intent.getAction(), SERVICE_ACTION_LOAD_ALL_PLAYER_RECORD)) {
-            LeaderBoardDAO leaderBoardDAO = new LeaderBoardDAO(this);
-            ArrayList<PlayerRecord> playerRecords = new ArrayList<>(leaderBoardDAO.getAllPlayerRecord());
-            Intent records = new Intent(SERVICE_ACTION_LOAD_ALL_PLAYER_RECORD);
-            records.putExtra("playerRecords", playerRecords);
-            sendBroadcast(intent);
+            getLeaderBoard();
         } else if (Objects.equals(intent.getAction(), SERVICE_ACTION_SAVE_GAME)) {
-
+            saveGameAchieve();
+        } else if (Objects.equals(intent.getAction(), SERVICE_ACTION_LOAD_GAME)) {
+            loadGameAchieve();
         }
         return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void savePlayerRecord(Intent intent) {
+        PlayerRecord playerRecord = (PlayerRecord) intent.getSerializableExtra("playerRecord");
+        if (playerRecord == null) {
+            throw new IllegalArgumentException("PlayerRecord cannot be null");
+        }
+        LeaderBoardDAO leaderBoardDAO = new LeaderBoardDAO(this);
+        boolean result = leaderBoardDAO.insertPlayerRecord(playerRecord);
+        Intent resultIntent = new Intent(SERVICE_ACTION_SAVE_PLAYER_RECORD);
+        resultIntent.putExtra("result", result);
+        sendBroadcast(resultIntent);
+    }
+
+    private void getLeaderBoard() {
+        LeaderBoardDAO leaderBoardDAO = new LeaderBoardDAO(this);
+        ArrayList<PlayerRecord> playerRecords = new ArrayList<>(leaderBoardDAO.getAllPlayerRecord());
+        Intent records = new Intent(SERVICE_ACTION_LOAD_ALL_PLAYER_RECORD);
+        records.putExtra("playerRecords", playerRecords);
+        sendBroadcast(records);
+    }
+
+    private void saveGameAchieve() {
+
+    }
+
+    private void loadGameAchieve() {
+
     }
 }
