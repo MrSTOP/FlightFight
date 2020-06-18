3428862e03d34b3208f6973188f2e9362072f95f
YzCatOvO <1315250388@qq.com>
2020-06-18 19:41:22 +0800

    增加启动app界面

--- app/src/main/java/com/flightfight/flightfight/MainActivity.java
+++ app/src/main/java/com/flightfight/flightfight/MainActivity.java
@@ -5,13 +5,18 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -25,6 +30,11 @@ public class MainActivity extends AppCompatActivity {
     private TextView quitGame;
     private ImageView musicControl;
     private ImageView pause;
+    private final int STOP_SPLASH = 0;
+    private final int SPLASH_TIME = 3000;
+
+    private ConstraintLayout splashLt;
+    private ConstraintLayout main_layout;
     private boolean isPlayMusic = true;
     public static final String TAG_EXIT = "exit";
 
@@ -38,6 +48,24 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+
+    private Handler splashHandler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case STOP_SPLASH:
+                    splashLt.setVisibility(View.GONE);
+                    main_layout.setVisibility(View.VISIBLE);
+                    break;
+                default:
+                    break;
+            }
+
+            super.handleMessage(msg);
+        }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +73,15 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        main_layout = findViewById(R.id.main_layout);
+        splashLt = findViewById(R.id.splash);
+        main_layout.setVisibility(View.GONE);
+        Message msg = new Message();
+        msg.what = STOP_SPLASH;
+
+        // 注：这里必须用延迟发送消息的方法，否则ImageView不会显示出来
+        splashHandler.sendMessageDelayed(msg, SPLASH_TIME);
+
         musicControl = findViewById(R.id.music_control);
         startGame = findViewById(R.id.main_start_game);
         aboutGame = findViewById(R.id.main_about_game);
